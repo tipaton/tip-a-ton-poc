7314c04182dbb88340c116b0dc3f3d1d61531e70
Bountie Tech <tipaton.dev@gmail.com>
2022-07-04 17:57:19 +0700

    Updating height on active stream

--- components/active-streaming.tsx
+++ components/active-streaming.tsx
@@ -15,7 +15,7 @@ export default function ActiveStreaming() {
               <div className='py-8 space-y-4 sm:space-y-0 sm:grid md:grid-cols-2 sm:gap-4 md:gap-8 lg:grid-cols-3'>
                 <Link href={"/streaming"}>
                   <div className="max-w-md bg-gradient-to-bl from-blue-400/30 to-slate-800 p-2 rounded-lg transition duration-200 ease-in transform md:hover:scale-105 hover:z-50">
-                    <div className="bg-black max-w-md rounded-lg overflow-hidden shadow-lg h-80 sm:max-w-sm sm:h-72 hover:cursor-pointer ">
+                    <div className="bg-black max-w-md rounded-lg overflow-hidden shadow-lg h-64 sm:max-w-sm sm:h-72 hover:cursor-pointer ">
                       <Image src={SampleML} alt="Mobile Legend Streaming" layout="responsive" className='rounded-t-lg'/>
                       <div className='flex items-center justify-between py-4 px-4 text-white text-xl font-bold'>
                         <span className='tracking-wider'>Jess No Limit</span>
@@ -26,7 +26,7 @@ export default function ActiveStreaming() {
                 </Link>
                 <Link href={"/streaming"}>
                   <div className="max-w-md bg-gradient-to-bl from-blue-400/30 to-slate-800 p-2 rounded-lg transition duration-200 ease-in transform md:hover:scale-105 hover:z-50">
-                    <div className="bg-black md:my-0 max-w-md rounded-lg overflow-hidden shadow-lg h-80 sm:max-w-sm md:h-72 hover:cursor-pointer">
+                    <div className="bg-black md:my-0 max-w-md rounded-lg overflow-hidden shadow-lg h-64 sm:max-w-sm md:h-72 hover:cursor-pointer">
                         <Image src={SampleML2} alt="Mobile Legend Streaming" layout="responsive" className='rounded-t-lg'/>
                         <div className='flex items-center justify-between py-4 px-4 text-white text-xl font-bold'>
                           <span className='tracking-wider'>Bang Bang</span>
@@ -37,7 +37,7 @@ export default function ActiveStreaming() {
                 </Link>
                 <Link href={"/streaming"}>
                   <div className="max-w-md bg-gradient-to-bl from-blue-400/30 to-slate-800 p-2 rounded-lg transition duration-200 ease-in transform md:hover:scale-105 hover:z-50">
-                    <div className="bg-black md:my-0 max-w-md rounded-lg overflow-hidden shadow-lg h-80 sm:max-w-sm md:h-72 hover:cursor-pointer">
+                    <div className="bg-black md:my-0 max-w-md rounded-lg overflow-hidden shadow-lg h-64 sm:max-w-sm md:h-72 hover:cursor-pointer">
                         <Image src={SampleML3} alt="Mobile Legend Streaming" layout="responsive" className='rounded-t-lg'/>
                         <div className='flex items-center justify-between py-4 px-4 text-white text-xl font-bold'>
                           <span className='tracking-wider'>Donkey Bar Bar</span>
