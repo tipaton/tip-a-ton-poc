09b9a3c11628238567cce2b006ec85f5bab3a83d
Bountie Tech <tipaton.dev@gmail.com>
2022-07-06 09:27:46 +0700

    Updating font size

--- components/ActiveStreaming.tsx
+++ components/ActiveStreaming.tsx
@@ -10,7 +10,7 @@ export default function ActiveStreaming() {
     return (
         <div className="px-6 py-6 sm:px-0">
             <div>
-              <h2 className='text-3xl font-bold text-white tracking-wide'>
+              <h2 className='text-2xl md:text-3xl font-bold text-white tracking-wide'>
                 Active Streaming
               </h2>
               <div className='py-8 space-y-8 sm:space-y-0 sm:grid md:grid-cols-2 sm:gap-4 md:gap-8 lg:grid-cols-3'>
@@ -18,7 +18,7 @@ export default function ActiveStreaming() {
                   <div className="max-w-md bg-gradient-to-bl from-blue-400/30 to-slate-800 p-2 rounded-lg transition duration-200 ease-in transform md:hover:scale-105 hover:z-50">
                     <div className="bg-black max-w-md rounded-lg overflow-hidden shadow-lg h-60 md:h-72 sm:max-w-sm hover:cursor-pointer ">
                       <Image src={SampleML} alt="Mobile Legend Streaming" layout="responsive" className='rounded-t-lg'/>
-                      <div className='flex items-center justify-between py-4 px-4 text-white text-xl font-bold'>
+                      <div className='flex items-center justify-between py-4 px-4 text-white text-lg md:text-xl font-bold'>
                         <span className='tracking-wider'>Jess No Limit</span>
                         <div className='text-blue-400 space-x-2'>
                           <Image src={SmallLogo} alt="Small Logo" height={16} width={16}/>
